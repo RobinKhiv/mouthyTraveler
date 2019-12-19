fcb0bf0548337a73ac4426fcb7e9d97d365221e4
Robin Khiv <robinkhiv@gmail.com>
2019-12-18 22:01:31 -0800

    boilerplate for blog page

--- src/Component/Routes/Blog.js
+++ src/Component/Routes/Blog.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react'
 export class Blog extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+              <section className="entry-header">
+                <h2>Title</h2>
+                <p>Tags</p>
+                <p>Leave a comment</p>
+              </section>
+              <section className="entry-content">
+                <p>content</p>
+              </section>
+              <section className="entry-footer">
+                <div>Share This</div>
+                <div class="prev-nav"></div>
+                <p>Leave a comment</p>
+              </section>
+            </React.Fragment>
         )
     }
 }
